383709fa33504a80a702778d7d54ab285749bed0
Kevin Dishner <92637769+kd-rgb@users.noreply.github.com>
2022-06-03 07:30:54 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d7d5dafd.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
